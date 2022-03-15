92cf3c4e2899e090ef8a37911c3e69eab3bdcc9a
rabsonsayendajnr@gmail.com <rabsonsayendajnr@gmail.com>
2022-03-15 03:40:52 -0700

    datatypes

--- 1.variables_datatypes.php
+++ 1.variables_datatypes.php
@@ -1,3 +1,13 @@
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#1. DATA TYPES
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+> string        > array
+> integer       > object
+> float         > NULL
+> Boolean       > resource
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#2. VARIABLES
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #use $ sign.
 #PHP is dynamically typed language
 $variablename = 5
