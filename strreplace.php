3da11bd544132e67b71b8e3cbc01815ee554bfa6
rabsonsayendajnr@gmail.com <rabsonsayendajnr@gmail.com>
2022-03-22 02:45:36 -0700

    replacecontinues 24

--- strreplace.php
+++ strreplace.php
@@ -1,3 +1,5 @@
 #replaces some characters with some other characters in a string
 #signature: str_replace(find,replace,string,[count])
 $ussdString = "123*9#3*4";
+// replacing # with *
+$newString = str_replace("#","*",$ussdString); 
